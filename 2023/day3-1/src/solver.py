a88c441486d4d08fd08906836b0fd6b693556876
k402xxxcenxxx <k402xxxcenxxx@gmail.com>
2024-10-09 16:44:49 +0000

    Day3-1: add correct code

---
+++ 2023/day3-1/src/solver.py
@@ -0,0 +1,52 @@
+def solver(filepath: str) -> int:
+
+    res = 0
+    # engine_schematic
+    data = []
+    with open(filepath, "r") as fd:
+        for line in fd.readlines():
+            data.append(line)
+
+    rows = len(data)
+    cols = len(data[0])
+
+    def sub_solver(y: int, x: int) -> tuple[int, int]:
+        is_valid = False
+        res = 0
+        x_offset = 0
+
+        while x + x_offset < cols:
+            cur_x = x + x_offset
+            x_offset += 1
+
+            if ord('0') <= ord(data[y][cur_x]) <= ord('9'):
+                # Check surrounding
+                for dy in [-1, 0 , 1]:
+                    for dx in [-1, 0 , 1]:
+                        if 0 <= cur_x + dx < cols and 0 <= y + dy < rows:
+                            # not period and not digits
+                            if data[y + dy][cur_x + dx] != '.' and data[y + dy][cur_x + dx] != '\n':
+                                if not ((ord('0') <= ord(data[y + dy][cur_x + dx]) <= ord('9'))):
+                                    is_valid = True
+                
+                # calculate number
+                res = 10 * res + int(data[y][cur_x])
+            else:
+                break
+
+        if not is_valid:
+            res = 0
+        else:
+            print(res)
+        return res, x_offset
+
+    y = 0
+    while y < rows:
+        x = 0
+        while x < cols:
+            sub_res, x_offset = sub_solver(y, x)
+            res += sub_res
+            x += x_offset
+        y += 1
+
+    return res
